1cd3a1306700bf8ccf52abb6716995cf37aa5ed1
kodonghwan1 <166992503+kodonghwan1@users.noreply.github.com>
2024-06-13 13:11:24 +0900

    Update door.ino

--- door.ino
+++ door.ino
@@ -1,58 +1,40 @@
-int cdsPin = A0;
 int motorIA = 5;
 int motorIB = 6;
-int swClose = 8;
-int swOpen = 9;
-
-int doorState = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(motorIA,OUTPUT);
-  pinMode(motorIB,OUTPUT);
-  pinMode(swClose,INPUT);
-  pinMode(swOpen,INPUT);
+  pinMode(motorIA, OUTPUT);
+  pinMode(motorIB, OUTPUT);
+ 
+  // 모터를 초기 상태로 멈춤
+  digitalWrite(motorIA, LOW);
+  digitalWrite(motorIB, LOW);
 }
 
 void loop() {
-  int cdsValue = analogRead(cdsPin);
-  Serial.println(cdsValue);
-  if(cdsValue <= 300) //밝은데
-  {
-    if(doorState == 0) //창문이 닫혀있으면
-    {
-      doorState = 1; //도어의 상태는 열림상태로 변경
-      digitalWrite(motorIA,HIGH);
-      digitalWrite(motorIB,LOW);
-      delay(5000);
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    if (command == 'c') { // 'c' 명령어로 닫기
+      closeMotor();
+    } else if (command == 'o') { // 'o' 명령어로 열기
+      openMotor();
+    } else if (command == 's') { // 's' 명령어로 정지
+      stopMotor();
     }
   }
-  else if(cdsValue >= 600) //어두운데
-  {
-    if(doorState == 1) //창문이 열려있으면
-    {
-      doorState = 0; //도어의 상태는 열림상태로 변경
-      digitalWrite(motorIA,LOW);
-      digitalWrite(motorIB,HIGH);
-      delay(5000);
-    }
-  }
+}
 
-  
-  if(digitalRead(swClose) == 0)
-  {
-    digitalWrite(motorIA,HIGH);
-    digitalWrite(motorIB,LOW);
-  }
-  else if(digitalRead(swOpen) == 0)
-  {
-    digitalWrite(motorIA,LOW);
-    digitalWrite(motorIB,HIGH);
+void openMotor() {
+  digitalWrite(motorIA, HIGH);
+  digitalWrite(motorIB, LOW);
+}
 
-  }
-  else
-  {
-    digitalWrite(motorIA,LOW);
-    digitalWrite(motorIB,LOW);
-  }
+void closeMotor() {
+  digitalWrite(motorIA, LOW);
+  digitalWrite(motorIB, HIGH);
+}
+
+void stopMotor() {
+  digitalWrite(motorIA, LOW);
+  digitalWrite(motorIB, LOW);
 }
